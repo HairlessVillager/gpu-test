284699ca85f86b29b1528dad470cb403223c3e6f
HairlessVillager <hairlessvillager@foxmail.com>
2024-10-11 10:43:54 +0800

    old version: main.py without batch

--- main.py
+++ main.py
@@ -1,73 +1,18 @@
 from time import time
-from typing import Annotated, List
+from typing import Union
+from typing_extensions import Annotated, List
 from logging import getLogger
 from uuid import uuid4
-import asyncio
-import os
 
-from fastapi import FastAPI, Body, Header
+from fastapi import FastAPI, HTTPException, Body, Header
 from pydantic import BaseModel
 from transformers import pipeline
 
 app = FastAPI()
 logger = getLogger("app")
-BATCH_TIMEOUT = float(os.getenv("BATCH_TIMEOUT"))
-BATCH_SIZE_MAX = int(os.getenv("BATCH_SIZE_MAX"))
-
-
-class DebugContext:
-
-    def __init__(self, uuid: str, start: float):
-        self.uuid = uuid
-        self.start = start
-        self.tags = {}
-
-    def debug(self, msg: str):
-        logger.debug(f"{self.uuid} after {time()-self.start:.6f}, {self.tags}: {msg}")
-
-    def tag(self, key: str, value):
-        new_ctx = DebugContext(uuid=self.uuid, start=self.start)
-        new_ctx.tags[key] = value
-        return new_ctx
-
-
-async def server_loop(mq: asyncio.Queue):
-    logger.info(f"{BATCH_TIMEOUT=}")
-    logger.info(f"{BATCH_SIZE_MAX=}")
-    logger.debug("loadding pipeline...")
-    pipe = pipeline(
-        "text-classification", model="TrustSafeAI/RADAR-Vicuna-7B", device="cuda:0"
-    )
-    logger.debug("loadding pipeline...done")
-    while True:
-        texts = []
-        rqs = []
-        try:
-            async with asyncio.timeout(BATCH_TIMEOUT):
-                for _ in range(BATCH_SIZE_MAX):
-                    text, rq, ctx = await mq.get()
-                    texts.append(text)
-                    rqs.append(rq)
-                    ctx.debug("received from mq")
-        except asyncio.TimeoutError:
-            pass
-
-        if texts:
-            batch_size = len(texts)
-            logger.debug("inferenceing...")
-            logger.info(f"{batch_size=}")
-            results = pipe(texts, batch_size=batch_size)
-            logger.debug("inferenceing...done")
-
-            for result, rq in zip(results, rqs):
-                rq.put_nowait(result)
-
-
-@app.on_event("startup")
-async def startup():
-    mq = asyncio.Queue()
-    app.mq = mq
-    asyncio.create_task(server_loop(mq))
+pipe = pipeline(
+    "text-classification", model="TrustSafeAI/RADAR-Vicuna-7B", device="cuda:0"
+)
 
 
 class DocumentResult(BaseModel):
@@ -81,14 +26,8 @@ class AIDetectionResponseModel(BaseModel):
     documents: List[DocumentResult]
 
 
-async def analyze_and_classify(text: str, ctx: DebugContext):
-    rq = asyncio.Queue()
-    ctx.debug("wait to put")
-    await app.mq.put((text, rq, ctx))
-    ctx.debug("put, wait to get")
-    output = await rq.get()
-    ctx.debug("got")
-    return output
+def analyze_and_classify(text: str):
+    return pipe(text)[0]
 
 
 def split_document_by_length(document: str, segment_length: int) -> List[str]:
@@ -99,31 +38,28 @@ def split_document_by_length(document: str, segment_length: int) -> List[str]:
 
 
 @app.post("/predit/text", response_model=AIDetectionResponseModel)
-async def ai_detection_on_single_string(
+def ai_detection_on_single_string(
     document: Annotated[str, Body()],
-    api_key: Annotated[str | None, Header(alias="x-api-key")] = None,
+    api_key: Annotated[Union[str, None], Header(alias="x-api-key")] = None,
     version: Annotated[str, Body()] = "no-version",
     multilingual: Annotated[bool, Body()] = False,
 ):
-    scanId = uuid4().hex
     start = time()
-    ctx = DebugContext(scanId, start)
-    ctx.debug(f"received request, length={len(document)}")
-
-    tasks = []
-    segments = split_document_by_length(document, 300)
-    for i, segment in enumerate(segments):
-        task = analyze_and_classify(segment, ctx.tag("id", i))
-        tasks.append(task)
-    results = await asyncio.gather(*tasks)
-
-    documents = [
-        {**result, "original_paragraph": segment}
-        for segment, result in zip(segments, results)
-    ]
-    ctx.debug("completed")
+    documents = []
+    for doc in split_document_by_length(document, 300):
+        result = analyze_and_classify(doc)
+        documents.append(
+            {
+                **result,
+                "original_paragraph": doc,
+            }
+        )
+    end = time()
+    cost = end - start
+    length = len(document)
+    logger.debug(f"{cost:.6f} / {length} = {cost / length :.6f}")
     return {
         "version": "no-version",
-        "scanId": scanId,
+        "scanId": uuid4().hex,
         "documents": documents,
     }
